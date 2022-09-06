0613f0193cfdbde5e4628383f589c8ac3e45a4cb
AryaArunB <arya2001bhosale@gmail.com>
2022-09-07 01:41:00 +0530

    matches done

--- client/src/pages/Modals.js
+++ client/src/pages/Modals.js
@@ -89,12 +89,13 @@ function Modals() {
       <div>
         <Button onClick={openModal}>Open modal</Button>
       </div>
+
       <Modal
         isOpen={isModalOpen}
         toggle={(e) => openModal(cardIndex)}
         onClose={closeModal}
       >
-        <div class="flex flex-col items-center pb-10">
+        <div class="flex flex-col items-center ">
           <div className="flex">
             <img
               class="mb-3 w-20 h-20 rounded-full shadow-lg mx-2"
@@ -103,33 +104,264 @@ function Modals() {
             />{" "}
             <img
               class="mb-3 w-20 h-20 rounded-full shadow-lg mx-2 object-scale-down"
-              src={cardIndex !== null && counter && counter[cardIndex].opponent_icon}
+              src={
+                cardIndex !== null &&
+                counter &&
+                counter[cardIndex].opponent_icon
+              }
               alt="Bonnie image"
             />
           </div>
         </div>
-        {cardIndex !== null && counter && counter[cardIndex].opponent}
-        <ModalHeader>
-          {cardIndex}
-          {/* {(cardIndex) => counter && counter[cardIndex].name} */}
-        </ModalHeader>
-        <ModalBody></ModalBody>
+        {/* {cardIndex !== null && counter && counter[cardIndex].opponent} */}
+        {/* <ModalHeader>
+          {(cardIndex) => counter && counter[cardIndex].name}
+        </ModalHeader> */}
+        <ModalBody>
+          <TableContainer className="mb-8">
+            <Table>
+              <TableHeader>
+                <tr>
+                  <TableCell>
+                    {team && team.map((player1) => player1.name)}
+                  </TableCell>
+                  <TableCell></TableCell>
+                  <TableCell>
+                    {cardIndex !== null &&
+                      counter &&
+                      counter[cardIndex].opponent}
+                  </TableCell>
+                </tr>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableCell>
+                    <span className="text-sm">
+                      <Badge type="success">
+                        {cardIndex !== null &&
+                          counter &&
+                          counter[cardIndex].goals}
+                      </Badge>
+                    </span>
+                  </TableCell>
+                  <TableCell>
+                    <span className="text-sm font-semibold">Goals</span>
+                  </TableCell>
+                  <TableCell>
+                    <span className="text-sm">
+                      <Badge type="warning">
+                        {cardIndex !== null &&
+                          counter &&
+                          counter[cardIndex].opponent_goals}
+                      </Badge>
+                    </span>
+                  </TableCell>
+                </TableRow>
+
+                <TableRow>
+                  <TableCell>
+                    <span className="text-sm">
+                      <Badge type="success">
+                        {cardIndex !== null &&
+                          counter &&
+                          counter[cardIndex].home_fouls}
+                      </Badge>
+                    </span>
+                  </TableCell>
+                  <TableCell>
+                    <span className="text-sm font-semibold">Fouls</span>
+                  </TableCell>
+                  <TableCell>
+                    <span className="text-sm">
+                      <Badge type="warning">
+                        {cardIndex !== null &&
+                          counter &&
+                          counter[cardIndex].away_fouls}
+                      </Badge>
+                    </span>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <span className="text-sm">
+                      <Badge type="success">
+                        {cardIndex !== null &&
+                          counter &&
+                          counter[cardIndex].home_yellow}
+                      </Badge>
+                    </span>
+                  </TableCell>
+                  <TableCell>
+                    <span className="text-sm font-semibold">Yellow Cards</span>
+                  </TableCell>
+                  <TableCell>
+                    <span className="text-sm">
+                      <Badge type="warning">
+                        {cardIndex !== null &&
+                          counter &&
+                          counter[cardIndex].away_yellow}
+                      </Badge>
+                    </span>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <span className="text-sm">
+                      <Badge type="success">
+                        {cardIndex !== null &&
+                          counter &&
+                          counter[cardIndex].home_red}
+                      </Badge>
+                    </span>
+                  </TableCell>
+                  <TableCell>
+                    <span className="text-sm font-semibold">Red Cards</span>
+                  </TableCell>
+                  <TableCell>
+                    <span className="text-sm">
+                      <Badge type="warning">
+                        {cardIndex !== null &&
+                          counter &&
+                          counter[cardIndex].away_red}
+                      </Badge>
+                    </span>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <span className="text-sm">
+                      <Badge type="success">
+                        {cardIndex !== null &&
+                          counter &&
+                          counter[cardIndex].home_passes}
+                      </Badge>
+                    </span>
+                  </TableCell>
+                  <TableCell>
+                    <span className="text-sm font-semibold">Passes</span>
+                  </TableCell>
+                  <TableCell>
+                    <span className="text-sm">
+                      <Badge type="warning">
+                        {cardIndex !== null &&
+                          counter &&
+                          counter[cardIndex].away_passes}
+                      </Badge>
+                    </span>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <span className="text-sm">
+                      <Badge type="success">
+                        {cardIndex !== null &&
+                          counter &&
+                          counter[cardIndex].home_tackles}
+                      </Badge>
+                    </span>
+                  </TableCell>
+                  <TableCell>
+                    <span className="text-sm font-semibold">Tackles</span>
+                  </TableCell>
+                  <TableCell>
+                    <span className="text-sm">
+                      <Badge type="warning">
+                        {cardIndex !== null &&
+                          counter &&
+                          counter[cardIndex].away_tackles}
+                      </Badge>
+                    </span>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <span className="text-sm">
+                      <Badge type="success">
+                        {cardIndex !== null &&
+                          counter &&
+                          counter[cardIndex].home_corners}
+                      </Badge>
+                    </span>
+                  </TableCell>
+                  <TableCell>
+                    <span className="text-sm font-semibold">Corners</span>
+                  </TableCell>
+                  <TableCell>
+                    <span className="text-sm">
+                      <Badge type="warning">
+                        {cardIndex !== null &&
+                          counter &&
+                          counter[cardIndex].away_corners}
+                      </Badge>
+                    </span>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <span className="text-sm">
+                      <Badge type="success">
+                        {cardIndex !== null &&
+                          counter &&
+                          counter[cardIndex].home_possession}
+                      </Badge>
+                    </span>
+                  </TableCell>
+                  <TableCell>
+                    <span className="text-sm font-semibold">Possession</span>
+                  </TableCell>
+                  <TableCell>
+                    <span className="text-sm">
+                      <Badge type="warning">
+                        {cardIndex !== null &&
+                          counter &&
+                          counter[cardIndex].away_possession}
+                      </Badge>
+                    </span>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <span className="text-sm">
+                      <Badge type="success">
+                        {cardIndex !== null &&
+                          counter &&
+                          counter[cardIndex].home_cross}
+                      </Badge>
+                    </span>
+                  </TableCell>
+                  <TableCell>
+                    <span className="text-sm font-semibold">Cross</span>
+                  </TableCell>
+                  <TableCell>
+                    <span className="text-sm">
+                      <Badge type="warning">
+                        {cardIndex !== null &&
+                          counter &&
+                          counter[cardIndex].away_cross}
+                      </Badge>
+                    </span>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </ModalBody>
         <ModalFooter>
           {/* I don't like this approach. Consider passing a prop to ModalFooter
            * that if present, would duplicate the buttons in a way similar to this.
            * Or, maybe find some way to pass something like size="large md:regular"
            * to Button
            */}
-          <div className="hidden sm:block">
-            <Button layout="outline" onClick={closeModal}>
-              Close
-            </Button>
-          </div>
           {/* <div className="hidden sm:block">
             <Button>Accept</Button>
           </div> */}
+          <div className="hidden sm:block">
+            <Button block size="regular" layout="outline" onClick={closeModal}>
+              Close
+            </Button>
+          </div>
           <div className="block w-full sm:hidden">
-            <Button block size="large" layout="outline" onClick={closeModal}>
+            <Button block size="regular" layout="outline" onClick={closeModal}>
               Close
             </Button>
           </div>
