c6c3995dd5ea8b79b01cf27d87da66f3e5fd9700
AryaArunB <arya2001bhosale@gmail.com>
2022-10-20 00:59:30 +0530

    added alert and compulsorised the upload of both the files in reactjs

--- client/src/pages/UploadVideo.js
+++ client/src/pages/UploadVideo.js
@@ -10,6 +10,7 @@ import {
   Dropdown,
   DropdownItem,
   HelperText,
+  Alert,
   Button,
 } from "@windmill/react-ui";
 import { ChartsIcon } from "../icons";
@@ -29,24 +30,27 @@ const myBucket = new AWS.S3({
 const UploadVideo = () => {
   const { user } = useAuthUser();
   const [progress, setProgress] = useState(0);
+  const [progress1, setProgress1] = useState(0);
+  const [change, setChange] = useState(false);
+  const [change1, setChange1] = useState(false);
   const [selectedFile, setSelectedFile] = useState(null);
-    const [selectedFile1, setSelectedFile1] = useState(null);
-
+  const [selectedFile1, setSelectedFile1] = useState(null);
 
   const handleFileInput = (e) => {
     setSelectedFile(e.target.files[0]);
+    setChange(true);
   };
-   const handleFileInput1 = (e) => {
-     setSelectedFile1(e.target.files[0]);
-   };
+  const handleFileInput1 = (e) => {
+    setSelectedFile1(e.target.files[0]);
+    setChange1(true);
+  };
 
   const uploadFile = (file) => {
     const params = {
       Body: file,
       Bucket: S3_BUCKET,
-      Key: user.username  + " " + Date(),
+      Key: user.username + " " + Date(),
     };
-    
 
     myBucket
       .putObject(params)
@@ -57,22 +61,22 @@ const UploadVideo = () => {
         if (err) console.log(err);
       });
   };
-const uploadFile1 = (file1) => {
-  const params = {
-    Body: file1,
-    Bucket: S3_BUCKET,
-    Key: user.username + " excel file " + Date(),
-  };
+  const uploadFile1 = (file1) => {
+    const params = {
+      Body: file1,
+      Bucket: S3_BUCKET,
+      Key: user.username + " excel file " + Date(),
+    };
 
-  myBucket
-    .putObject(params)
-    .on("httpUploadProgress", (evt1) => {
-      setProgress(Math.round((evt1.loaded / evt1.total) * 100));
-    })
-    .send((err) => {
-      if (err) console.log(err);
-    });
-};
+    myBucket
+      .putObject(params)
+      .on("httpUploadProgress", (evt1) => {
+        setProgress1(Math.round((evt1.loaded / evt1.total) * 100));
+      })
+      .send((err) => {
+        if (err) console.log(err);
+      });
+  };
   return (
     <div>
       <PageTitle>Upload Video</PageTitle>
@@ -85,7 +89,7 @@ const uploadFile1 = (file1) => {
           <Label>
             <span>Video</span>
             <p className="text-gray-600 dark:text-gray-400">
-              <Input type="file" onChange={handleFileInput} />
+              <Input type="file" onChange={handleFileInput} required />
             </p>
             <HelperText valid>
               The Video uploading progress is {progress}%
@@ -103,23 +107,70 @@ const uploadFile1 = (file1) => {
           <Label>
             <span>Spreadsheet</span>
             <p className="text-gray-600 dark:text-gray-400">
-              <Input type="file" onChange={handleFileInput1} />
+              <Input type="file" onChange={handleFileInput1} required />
             </p>
             <HelperText valid>
-              The Spreadsheet uploading progress is {progress}%
+              The Spreadsheet uploading progress is {progress1}%
             </HelperText>
           </Label>
           <br />
           <br />
-          <Button
-            onClick={() => {
-              uploadFile(selectedFile);
-              uploadFile1(selectedFile1);
-            }}
-          >
-            {" "}
-            Upload to S3
-          </Button>{" "}
+          {change1 && change ? (
+            <>
+              {" "}
+              <Button
+                onClick={() => {
+                  uploadFile(selectedFile);
+                  uploadFile1(selectedFile1);
+                }}
+              >
+                {" "}
+                Upload Files
+              </Button>{" "}
+            </>
+          ) : (
+            <>
+              {" "}
+              <Button
+                disabled
+                onClick={() => {
+                  uploadFile(selectedFile);
+                  uploadFile1(selectedFile1);
+                }}
+              >
+                {" "}
+                Upload Files
+              </Button>{" "}
+            </>
+          )}
+          {progress + progress1 == 200 ? (
+               (setTimeout(function () {
+                  window.location.reload();
+                }, 3000)) &&
+            <>
+                {" "}
+              <div
+                class="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex"
+                role="alert"
+              >
+                <span class="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">
+                  New
+                </span>
+                <span class="font-semibold mr-2 text-left flex-auto">
+                  File has been transferred sucessfully.
+                </span>
+                <svg
+                  class="fill-current opacity-75 h-4 w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
+                </svg>
+              </div>
+            </>
+          ) : (
+            <></>
+          )}
         </CardBody>
       </Card>
     </div>
